31d3dfbb464307a2524980baad33daf9d56700ae
Kevin Cearns <kcearns@gmail.com>
2023-03-07 09:02:00 -0500

    Setup tailwindcss

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Header from '../components/Header'
 
 const index = () => {
   return (
-    <div>
+    <>
+      <Header />
       <h1>Blocknodes Network</h1>
-    </div>
+    </>
   )
 }
 
